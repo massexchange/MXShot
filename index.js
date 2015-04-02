d0c8c2807061642554648e81a1891df06c285c61
Eugene Dobry <eugene@dobry.me>
2015-04-01 20:38:32 -0400

    implemented logging in

--- index.js
+++ index.js
@@ -9,26 +9,35 @@ page.onLoadFinished = function(status) {
 	page.evaluate(function() {
 		require(["app/hub"], function(hub) {
 			hub.sub("app/renderer/rendered", function(view) {
-				if(view == "login")
-					window.callPhantom();
+				window.callPhantom(view);
 			});
 		});
 		return document.getElementById("btnLogin");
 	});
 };
-page.onCallback = function() {
-	console.log("login form rendered!");
-	var btnLogin = page.evaluate(function() {
-		document.getElementById("txtUser").value = "pub";
-		document.getElementById("txtPass").value = "test";
-		return document.getElementById("btnLogin");
-	});
-	
-	page.sendEvent("click", btnLogin.offsetLeft, btnLogin.offsetTop);
 
-	while(!page.render('mx.png')) {}
-	phantom.exit();
+page.onCallback = function(view) {
+	console.log(view);
+	var viewActions = {
+		login: function() {
+			console.log("login form rendered!");
+			var btnLogin = page.evaluate(function() {
+				document.getElementById("txtUser").value = "pub";
+				document.getElementById("txtPass").value = "test";
+				document.getElementById("btnLogin").click();
+			});
+			// page.sendEvent("click", btnLogin.offsetLeft+5, btnLogin.offsetTop+5);
+		},
+		profile: function() {
+			while(!page.render('mx.png')) {}
+			phantom.exit();
+		}
+	};
+	
+	if(viewActions[view])
+		viewActions[view]();
 };
+
 
 
 page.open('http://localhost/profile');
